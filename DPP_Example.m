f66ceeb2219735071c8dce6097042269c84c82df
czechner <zechner@control.ee.ethz.ch>
2014-01-03 18:03:38 +0100

    GNU GPL license added

--- DPP_Example.m
+++ DPP_Example.m
@@ -5,7 +5,23 @@
 % heterogeneous gene expression kinetics using synthetic data of multiple
 % cells.
 %--------------------------------------------------------------------------
-% Author: Christoph Zechner, Automatic Control Lab, ETH Zurich
+% DPP - A MATLAB toolbox for the inference of heterogeneous reaction 
+% kinetics.
+%
+% Copyright (C) 2014  Christoph Zechner
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %--------------------------------------------------------------------------
 % For feedback and questions please contact me at:
 %   zechner@control.ee.ethz.ch
